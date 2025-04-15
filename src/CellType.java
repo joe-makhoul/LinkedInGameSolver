dad4f229f1b49d3f6917f6dcccf5f7e877807e22
joe-makhoul <joemakhoul.pro@gmail.com>
2025-04-15 22:05:44 +0200

    First commit - created enums for cell and edge types

---
+++ src/CellType.java
@@ -0,0 +1,17 @@
+/**
+ * Types of cell icons
+ */
+public enum CellType {
+    /**
+     * Empty cell
+     */
+    EMPTY,
+    /**
+     * Sun icon or equivalent
+     */
+    SUN,
+    /**
+     * Moon icon or equivalent
+     */
+    MOON
+}
---
+++ src/EdgeType.java
@@ -0,0 +1,17 @@
+/**
+ * Types of grid edges
+ */
+public enum EdgeType {
+    /**
+     * Normal edge
+     */
+    NORMAL,
+    /**
+     * Edge with equal sign
+     */
+    EQUALS,
+    /**
+     * Edge with a cross
+     */
+    CROSS
+}
